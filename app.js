63ff190424bebab18da388cb89cedef702dc6210
Shannon2016 <shannonsn@qq.com>
2019-05-16 17:37:17 +0800

    ..
    ..

--- app.js
+++ app.js
@@ -8,9 +8,49 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        ///
+      success: function (res) {
+        if (res.code) {
+          //获取openId
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              //小程序唯一标识
+              appid: 'wx0804bb250eb2a991',
+              //小程序的 app secret
+              secret: 'e2e51de816a65ace8e6fb33e36eab3fe',
+              grant_type: 'authorization_code',
+              js_code: res.code
+            },
+            method: 'GET',
+            header: { 'content-type': 'application/json' },
+            success: function (openIdRes) {
+              console.info("登录成功返回的openId：" + openIdRes.data.openid);
+              // weChatUserInfo.openId = openIdRes.data.openid;
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+                // 有一点需要注意 询问用户 是否授权 那提示 是这API发出的
+                wx.getUserInfo({
+                  success: function (data) {
+                    // 自定义操作
+                    // 绑定数据，渲染页面
+                    that.setData({
+
+                    });
+                  },
+                  fail: function (failData) {
+                    console.info("用户拒绝授权");
+                  }
+                });
+              } else {
+                console.info("获取用户openId失败");
+              }
+            },
+            fail: function (error) {
+              console.info("获取用户openId失败");
+              console.info(error);
+            }
+          })
+        }
       }
     })
     // 获取用户信息
