95e60657b9d855712181b234f698e16601ddfee8
Shannon2016 <shannonsn@qq.com>
2019-05-19 00:18:38 +0800

    2019/5/19 0:17 新增了确认支付页，增加了购物车页计算功能及跳转功能

--- app.js
+++ app.js
@@ -8,9 +8,49 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        ///
+      success: function (res) {
+        if (res.code) {
+          //获取openId
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              //小程序唯一标识
+              appid: 'wx75221c8ecddbf3d2',
+              //小程序的 app secret
+              secret: '856aea8a54f6b06b10f13af88ddc8364',
+              grant_type: 'authorization_code',
+              js_code: res.code
+            },
+            method: 'GET',
+            header: { 'content-type': 'application/json' },
+            success: function (openIdRes) {
+              console.info("登录成功返回的openId：" + openIdRes.data.openid);
+              // weChatUserInfo.openId = openIdRes.data.openid;
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+                // 有一点需要注意 询问用户 是否授权 那提示 是这API发出的
+                wx.getUserInfo({
+                  success: function (data) {
+                    // 自定义操作
+                    // 绑定数据，渲染页面
+                    // that.setData({
+
+                    // });
+                  },
+                  fail: function (failData) {
+                    console.info("用户拒绝授权");
+                  }
+                });
+              } else {
+                console.info("获取用户openId失败");
+              }
+            },
+            fail: function (error) {
+              console.info("获取用户openId失败");
+              console.info(error);
+            }
+          })
+        }
       }
     })
     // 获取用户信息
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,98 +1,53 @@
-// pages/cart/cart.js
-
+// pages/order/order.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    chooseItemIndex:[],
-    checkFlag: false,
-    totalPrice:0,
-    topImg:{
-      mode:'scaleToFill',
-      src:'../../image/6.jpg'
-    },
-    cartInfoList:[{
+    totalPrice:1116.50,
+    inputName:'',
+    inputPhone:'',
+    inputAddress:'',
+    orderInfoList: [{
         picSrc: "../../image/book1.png",
         bookName: "共产党宣言",
-        author:"cuteBug",
-        press:"BIT",
-        price:15.5
-      },{
+        author: "cuteBug",
+        press: "BIT",
+        price: 15.50
+      }, {
         picSrc: "../../image/book11.png",
         bookName: "博弈论",
         author: "cuteBug",
         press: "BIT",
-        price: 16
-      },{
+        price: 16.00
+      }, {
         picSrc: "../../image/book18.png",
         bookName: "围城",
         author: "cuteBug",
         press: "BIT",
-        price: 17
-      },{
+        price: 17.00
+      }, {
         picSrc: "../../image/book4.png",
         bookName: "中国哲学史",
         author: "cuteBug",
         press: "BIT",
-        price: 18.7
+        price: 18.70
       }, {
         picSrc: "../../image/book4.png",
         bookName: "图书4",
         author: "cuteBug",
         press: "BIT",
-        price: 18.7
+        price: '18.70'
       }
     ]
   },
 
-  getSum(){
-    var tmp = 0;
-    for (var i = 0; i < this.data.chooseItemIndex.length; i++) {
-      var index = parseInt(this.data.chooseItemIndex[i]);
-      tmp += this.data.cartInfoList[index].price;
-    }
-    tmp = tmp.toFixed(2)
-    this.setData({
-      totalPrice: tmp
-    })
-  },
-  checkAll: function(){
-    if (this.data.checkFlag === false){
-      this.setData({
-        checkFlag:true
-      })
-      this.data.chooseItemIndex = [];
-      for (var i = 0; i < this.data.cartInfoList.length; i ++){
-        this.data.chooseItemIndex.push(i.toString());
-      }
-      this.getSum();
-    }else{
-      this.setData({
-        checkFlag:false
-      })
-      this.data.chooseItemIndex = [];
-      this.setData({
-        totalPrice: 0
-      })
-    }
-  },
-  checkboxChange: function (e) {
-    this.data.chooseItemIndex=e.detail.value;
-    console.log(this.data.chooseItemIndex)
-    this.getSum();
-  },
-  goOrder:function(){
-    wx.navigateTo({
-      url: '../order/order',
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      
+
   },
 
   /**
