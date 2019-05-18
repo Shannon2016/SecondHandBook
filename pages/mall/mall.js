e1c456aea63ac5a8c3d0f010bfceb28a1bd98902
InspAlgo <InspAlgo@gmail.com>
2019-05-18 23:22:15 +0800

    完成图书详情页面

--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -1,90 +1,50 @@
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      books: [
-          {
-              picSrc: "../../image/book1.png",
-              name: "共产党宣言",
-              author: "马克思 恩格斯",
-              price: "15.00"
-          },
-          {
-              picSrc: "../../image/book11.png",
-              name: "博弈论",
-              author: "让·梯若尔",
-              price: "16.00"
-          },
-          {
-              picSrc: "../../image/book18.png",
-              name: "围城",
-              author: "钱钟书",
-              price: "17.00"
-          },
-          {
-              picSrc: "../../image/book4.png",
-              name: "中国哲学史",
-              author: "冯友兰",
-              price: "18.00"
-          },
-      ]
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        books: [{
+                picSrc: "/image/book1.png",
+                name: "共产党宣言",
+                author: "马克思 恩格斯",
+                price: "15.00",
+                description: "双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本",
+                level: 1,
+            },
+            {
+                picSrc: "/image/book11.png",
+                name: "博弈论",
+                author: "让·梯若尔",
+                price: "16.00",
+                description: "双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本",
+                level: 6,
+            },
+            {
+                picSrc: "/image/book18.png",
+                name: "围城",
+                author: "钱钟书",
+                price: "17.00",
+                description: "双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本",
+                level: 4,
+            },
+            {
+                picSrc: "/image/book4.png",
+                name: "中国哲学史",
+                author: "冯友兰",
+                price: "18.00",
+                description: "双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本双击编辑文本",
+                level: 3,
+            },
+        ]
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    showBookDetail: function(event) {
+        var bookIndex = event.currentTarget.dataset.bookIndex
+        var bookDetail = JSON.stringify(this.data.books[bookIndex])
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
+        wx.navigateTo({
+            url: '/pages/mall/book-detail/book-detail?bookDetail=' + escape(bookDetail),
+        })
+    }
 })
