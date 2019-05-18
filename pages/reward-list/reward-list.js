b306357637ec9be5d1a61261683abf9fea8c9836
InspAlgo <InspAlgo@gmail.com>
2019-05-18 15:47:58 +0800

    完成悬赏页面框架搭建

--- pages/reward-list/reward-list.js
+++ pages/reward-list/reward-list.js
@@ -4,7 +4,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+      books: [
+          {
+              picSrc: "../../image/book1.png",
+              name: "共产党宣言",
+              author: "马克思 恩格斯",
+              press:"xxx",
+              price: "15.00"
+          },
+          {
+              picSrc: "../../image/book11.png",
+              name: "博弈论",
+              author: "让·梯若尔",
+              press: "xxx",
+              price: "16.00"
+          },
+          {
+              picSrc: "../../image/book18.png",
+              name: "围城",
+              author: "钱钟书",
+              press: "xxx",
+              price: "17.00"
+          },
+          {
+              picSrc: "../../image/book4.png",
+              name: "中国哲学史",
+              author: "冯友兰",
+              press: "xxx",
+              price: "18.00"
+          },
+      ]
   },
 
   /**
