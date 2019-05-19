58500ec69b5514f63677e463a17fc7be74eccb12
InspAlgo <InspAlgo@gmail.com>
2019-05-19 16:39:30 +0800

    合并上个人中心和我的订单页面

---
+++ pages/personcenter/personcenter.js
@@ -0,0 +1,37 @@
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        userInfo: {},
+        hasUserInfo: false,
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        var userInfo = wx.getStorageSync('userInfo')
+        this.setData({
+            userInfo: userInfo
+        })
+    },
+
+    catchToMyOrderTap: function() {
+        wx.navigateTo({
+            url: '/pages/personcenter/myorder/myorder',
+        })
+    },
+
+    catchToMyRewardTap: function () {
+        wx.navigateTo({
+            url: '',
+        })
+    },
+
+    catchToMySellTap: function () {
+        wx.navigateTo({
+            url: '',
+        })
+    },
+})
